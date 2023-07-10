997817a0edbb1bbb356db44bdd966305c1e95d72
Paloma Tejeda <palomatejeda81@gmail.com>
2023-07-10 18:03:18 +0200

    mc

--- src/pages/a-propos.vue
+++ src/pages/a-propos.vue
@@ -19,7 +19,7 @@ const breadcrumbs = [
       <ContactFormComponent />
       <iframe
         class="map"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2766.052794322215!2d5.975977115453415!3d46.10985497911395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478c80986db68cb9%3A0x105ec46440dff85c!2s730%20Rte%20de%20Saint-Julien%2C%2074520%20Valleiry!5e0!3m2!1sfr!2sfr!4v1678899735904!5m2!1sfr!2sfr"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2797.595292715556!2d6.043295085975591!3d45.47795589649109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478bb28a6d393433%3A0x9ae564f9711ac99!2s508%20Rue%20pichat%2C%2073800%20Sainte-H%C3%A9l%C3%A8ne-du-Lac!5e0!3m2!1sfr!2sfr!4v1689004905526!5m2!1sfr!2sfr"
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"
       ></iframe>
