7d3ed59b5141455efce84610ffec2089b33c3c95
Paloma Tejeda <palomatejeda81@gmail.com>
2023-11-28 15:15:29 +0100

    payment testing with snipcart

--- src/components/home/PricingComponent.vue
+++ src/components/home/PricingComponent.vue
@@ -48,6 +48,7 @@ onMounted(() => {
         data-item-custom3-name="J'ai besoin d'une charte graphique"
         data-item-custom3-type="checkbox"
         data-item-custom4-name="Adresse mail à laquelle vous souhaitez recevoir le devis"
+        data-item-url="https://www.tekilawebfactory.com"
       >
         <div class="pricing__cards__card__headlines">
           <h3 class="pricing__cards__card__headlines__title">Site vitrine</h3>
@@ -127,6 +128,7 @@ onMounted(() => {
         data-item-custom3-name="J'ai besoin d'une charte graphique"
         data-item-custom3-type="checkbox"
         data-item-custom4-name="Adresse mail à laquelle vous souhaitez recevoir le devis"
+        data-item-url="https://www.tekilawebfactory.com"
       >
         <div class="pricing__cards__card__headlines">
           <h3 class="pricing__cards__card__headlines__title">Site E-commerce</h3>
@@ -216,6 +218,7 @@ onMounted(() => {
         data-item-custom1-name="URL de votre site internet"
         data-item-custom2-name="Adresse mail à laquelle vous souhaitez être contacté(e)"
         data-item-custom3-name="Numéro auquel vous souhaitez être contacté(e) (uniquement en cas de problème, nous privilégions le contact par mail)"
+        data-item-url="https://www.tekilawebfactory.com"
       >
         <div class="pricing__cards__card__headlines">
           <h3 class="pricing__cards__card__headlines__title">SEO & Performance</h3>
